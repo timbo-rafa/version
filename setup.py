a38b7c2c31c57c417edebd42508fa167732e2722
Rafael <timbo.rafa@gmail.com>
2020-02-15 18:27:24 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Version Comparator",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/timbo-rafa",
+    url="https://github.com/timbo-rafa/version",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
